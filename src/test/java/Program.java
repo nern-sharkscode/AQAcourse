7ee186ac47cf2e2ad8f2534cf6380d0579971c2a
nern-sharkscode <nern@sharkscode.com>
2026-07-26 16:37:54 +0200

    Change the message to create pin

--- src/test/java/Program.java
+++ src/test/java/Program.java
@@ -70,7 +70,7 @@ public class Program {
         System.out.println("Creating a new account...");
         System.out.println("Enter the Fullname");
         user.setFullName(scanner.nextLine());
-        System.out.println("Create the pin");
+        System.out.println("Create the pin to safety use your data");
         user.setPin(scanner.nextLine());
         System.out.println("Account for " + user.getFullName() + " is created!\n The account number = " + user.getAccountNumber());
         return;
