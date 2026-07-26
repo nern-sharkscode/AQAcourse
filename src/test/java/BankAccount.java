0d0e601744d17a5985a802e1eea034a24a212dc3
nern-sharkscode <nern@sharkscode.com>
2026-07-26 15:29:05 +0200

    My first comit

---
+++ src/test/java/BankAccount.java
@@ -0,0 +1,52 @@
+public class BankAccount {
+
+    long min = 1_000_000_000_000_000L;
+    long max = 9_999_999_999_999_999L;
+    private Long accountNumber = min + (long) (Math.random() * (max - min + 1));
+    private String fullName;
+    private double balance = 0.0;
+    private String pin;
+
+    public void addMoney(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Amount of money can't be less than zero");
+        }
+    }
+
+    public void withdrawMoney(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("You don't have enough money on your balance");
+        }
+    }
+
+    public boolean checkPin(String userUncheckedPin) {
+        if (userUncheckedPin.equals(this.pin)) {
+            return true;
+        } else
+            return false;
+    }
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+}
---
+++ src/test/java/Program.java
@@ -0,0 +1,77 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BankAccount shortName = new BankAccount();
+        createAccount(shortName, scanner);
+        while (true) {
+            System.out.println("Choose an option:\n" +
+                    "1. Enter 1 - to check the balance\n" +
+                    "2. Enter 2 - to add money\n" +
+                    "3. Enter 3 - to withdraw money");
+            switch (scanner.nextLine()) {
+                case "1":
+                    if (verifyPin(shortName, scanner)) {
+                        System.out.println("Your balance = " + shortName.getBalance() + "\n");
+                    }
+                    break;
+
+                case "2":
+                    if (verifyPin(shortName, scanner)) {
+                        System.out.println("Enter amount of money");
+                        double amount = Double.parseDouble(scanner.nextLine());
+                        shortName.addMoney(amount);
+                    }
+                    break;
+
+                case "3":
+                    if (verifyPin(shortName, scanner)) {
+                        System.out.println("Enter amount of money");
+                        double amountForWithdraw = Double.parseDouble(scanner.nextLine());
+                        shortName.withdrawMoney(amountForWithdraw);
+                    }
+                    break;
+                default:
+                    System.out.println("Incorrect option, try again!");
+            }
+        }
+    }
+
+    public static boolean verifyPin(BankAccount user, Scanner scanner) {
+        boolean result;
+        while (true) {
+            System.out.println("Enter the pin");
+
+            if (user.checkPin(scanner.nextLine())) {
+                result = true;
+                break;
+            } else {
+                System.out.println("Wrong password!\n Enter 1 - to try again\n Enter 2 - for exit to main menu");
+                String userSubMenuChoice = scanner.nextLine();
+                if (userSubMenuChoice.equals("1")) {
+                    continue;
+                } else {
+                    if (userSubMenuChoice.equals("2")) {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    ;
+
+    public static void createAccount(BankAccount user, Scanner scanner) {
+        System.out.println("Creating a new account...");
+        System.out.println("Enter the Fullname");
+        user.setFullName(scanner.nextLine());
+        System.out.println("Create the pin");
+        user.setPin(scanner.nextLine());
+        System.out.println("Account for " + user.getFullName() + " is created!\n The account number = " + user.getAccountNumber());
+        return;
+    }
+}
