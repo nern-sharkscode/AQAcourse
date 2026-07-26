d41a4bd5306ccd5006355d2f30b1319b4d79e2ad
nern-sharkscode <nern@sharkscode.com>
2026-07-26 15:43:21 +0200

    Add welcome message

--- src/test/java/Program.java
+++ src/test/java/Program.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Java Bank ATM!");
         BankAccount shortName = new BankAccount();
         createAccount(shortName, scanner);
         while (true) {
