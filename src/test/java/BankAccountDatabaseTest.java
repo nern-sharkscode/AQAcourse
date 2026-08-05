41407009459474f4d41163c7b0435b82502665bb
nern-sharkscode <nern@sharkscode.com>
2026-08-05 23:06:55 +0200

    Fixed comments from mentor

--- src/test/java/BankAccountDatabaseTest.java
+++ src/test/java/BankAccountDatabaseTest.java
@@ -8,46 +8,45 @@ public class BankAccountDatabaseTest {
     private BankAccount testAccount;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         db = new BankAccountsDatabase();
         testAccount = new BankAccount();
         testAccount.setPin("7890");
     }
 
     @AfterMethod
-    public void testFinish(){
+    public void testFinish() {
         System.out.println("The test is finished");
     }
 
-    @Test(groups = "database")
-    public void testInitialDatabaseSize(){
+    @Test(groups = "positive")
+    public void testInitialDatabaseSize() {
         Assert.assertEquals(db.getDatabaseSize(), 0, "Database already has some data!");
     }
 
-    @Test(groups = "database")
-    public void oneAccountDatabaseSizeTest(){
+    @Test(groups = "positive")
+    public void oneAccountDatabaseSizeTest() {
         TestDataHelper.addAccountToDB(testAccount, db);
         Assert.assertEquals(db.getDatabaseSize(), 1, "Database size is not equal 1");
     }
 
-    @Test(groups = "database")
-    public void databaseHasAccountTest(){
+    @Test(groups = "positive")
+    public void databaseHasAccountTest() {
         TestDataHelper.addAccountToDB(testAccount, db);
         Assert.assertTrue(db.containsAccount(testAccount), "Database doesn't have that account");
     }
 
-    @Test(groups = "database")
-    public void getDatabaseSizeAfterRemoveAccountTest(){
+    @Test(groups = "positive")
+    public void getDatabaseSizeAfterRemoveAccountTest() {
         TestDataHelper.addAccountToDB(testAccount, db);
         db.removeAccount(testAccount);
         Assert.assertEquals(db.getDatabaseSize(), 0, "Account wasn't removed from Database");
     }
 
-    @Test(groups = "database")
-    public void databaseNotContainsRemovedAccountTest(){
+    @Test(groups = "positive")
+    public void databaseNotContainsRemovedAccountTest() {
         TestDataHelper.addAccountToDB(testAccount, db);
         db.removeAccount(testAccount);
         Assert.assertFalse(db.containsAccount(testAccount), "Database still contain account");
     }
-
 }
